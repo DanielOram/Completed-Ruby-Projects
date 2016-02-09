87b9ef67f5533df47194dd88591132e52faafcc1
Danoram <danieloramnz@gmail.com>
2016-02-09 18:55:42 +1300

    merge conflict resolved

--- Photogram/app/models/post.rb
+++ Photogram/app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
+  
   #check the presence of an image - cannot be null
   validates :image, presence: true
-  has_attached_file :image, styles: {:medium => "640px"}
-  
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  has_attached_file :image, styles: {medium => "640px"}
+    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
