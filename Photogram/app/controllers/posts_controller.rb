b49e3891529b98a43a79a0f57c21856b0f5e5fa5
Danoram <danieloramnz@gmail.com>
2016-02-09 18:18:29 +1300

    add form to view and appropriate code to model/controller

--- Photogram/app/controllers/posts_controller.rb
+++ Photogram/app/controllers/posts_controller.rb
@@ -3,7 +3,22 @@ class PostsController < ApplicationController
     
   end
   
+  #upload new image
   def new 
-    
+    @post = Post.new
   end
+  
+  #create action for submitting a post
+  def create
+    @post = Post.create( post_params )
+    #redirect to posts_path
+    redirect_to posts_path
+  end
+  
+  #define post_params
+  private
+
+  def post_params  
+    params.require(:post).permit(:image, :caption)
+  end 
 end
