55ce316f6e75f8aa0bb2c4dbb7f0efe46f0219f3
Danoram <danieloramnz@gmail.com>
2016-02-09 16:49:35 +1300

    add image validation and formatting to Post class

--- Photogram/app/models/post.rb
+++ Photogram/app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  #check the presence of an image - cannot be null
+  validates :image, presence: true
+  has_attached_file :image, styles: {medium => "640px"}
+  #has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
