526d6197b6a2fd694b0dc08f3d0ebb7b31f6595b
Danoram <danieloramnz@gmail.com>
2016-02-09 18:48:28 +1300

    faulty commit

--- Photogram/db/schema.rb
+++ Photogram/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160209030607) do
+ActiveRecord::Schema.define(version: 0) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "caption"
