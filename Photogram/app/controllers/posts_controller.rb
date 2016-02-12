2f147930b551fe3aee7c547084266fdb0e485c7a
Danoram <danieloramnz@gmail.com>
2016-02-12 18:55:17 +1300

    setup create action in posts controller

--- Photogram/app/controllers/posts_controller.rb
+++ Photogram/app/controllers/posts_controller.rb
@@ -3,6 +3,13 @@ class PostsController < ApplicationController
   end
   
   def new 
-    @post = Post.new
+    @post = Post.create(post_params)
+    redirect_to posts_path
   end
+  
+  private
+
+  def post_params  
+    params.require(:post).permit(:image, :caption)
+  end  
 end
